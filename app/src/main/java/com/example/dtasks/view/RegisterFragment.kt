664e86ee5bad357b877709ab92eb1e1333989a7c
Danbuth <buthdan10@gmail.com>
2025-04-07 19:02:18 -0600

    Creando pantalla de Registro

--- app/src/main/java/com/example/dtasks/view/RegisterFragment.kt
+++ app/src/main/java/com/example/dtasks/view/RegisterFragment.kt
@@ -12,7 +12,7 @@ import com.example.dtasks.databinding.FragmentSecondBinding
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class RegisterFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
 
