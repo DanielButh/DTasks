1529583f7844a5c929b7c00244065b57bddb1cff
Danbuth <buthdan10@gmail.com>
2025-04-29 23:47:53 -0600

    Creación de layouts de ListActivity

--- app/src/main/java/com/example/dtasks/view/onboarding/LoginFragment.kt
+++ app/src/main/java/com/example/dtasks/view/onboarding/LoginFragment.kt
@@ -1,16 +1,19 @@
 package com.example.dtasks.view.onboarding
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.dtasks.R
 import com.example.dtasks.databinding.FragmentLoginBinding
 import com.example.dtasks.utils.FragmentCommunicator
+import com.example.dtasks.view.list.ListActivity
 import com.example.dtasks.viewModel.LoginViewModel
 
 /**
@@ -43,13 +46,43 @@ class LoginFragment : Fragment() {
         binding.registerTB.setOnClickListener {
             findNavController().navigate(R.id.action_LoginFragment_to_RegisterFragment)
         }
+
+        binding.loginButton.setOnClickListener {
+            requestLogin()
+        }
     }
 
     private fun setupObservers() {
         viewModel.loaderState.observe(viewLifecycleOwner) { loaderState ->
             communicator.showLoader(loaderState)
         }
+        viewModel.sessionValid.observe(viewLifecycleOwner) {sessionValid ->
+            if (sessionValid) {
+                val intent = Intent(activity, ListActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
+            } else {
+                Toast.makeText(activity, "invalid access", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
+    private fun requestLogin() {
+        if (binding.emailTIET.text.toString().isNotEmpty()
+            && binding.passwordTIET.text.toString().isNotEmpty()){
+            isValid = true
+        } else {
+            isValid = false
+        }
+
+        if (isValid){
+            viewModel.requestLogin(
+                binding.emailTIET.text.toString(),
+                binding.passwordTIET.text.toString()
+            )
+        } else {
+            Toast.makeText(activity, "please introduce your information", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/dtasks/view/onboarding/RegisterFragment.kt
+++ app/src/main/java/com/example/dtasks/view/onboarding/RegisterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.dtasks.R
@@ -23,6 +24,7 @@ class RegisterFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     private val viewModel by viewModels<RegisterViewModel>()
+    var isValid: Boolean = false
     private lateinit var communicator: FragmentCommunicator
 
     override fun onCreateView(
@@ -32,6 +34,7 @@ class RegisterFragment : Fragment() {
 
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         setupView()
+        setupObservers()
         return binding.root
 
     }
@@ -40,12 +43,38 @@ class RegisterFragment : Fragment() {
         binding.backIB.setOnClickListener {
             findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
         }
-        setupObservers()
+
+        binding.registerButton.setOnClickListener {
+            requestRegister()
+        }
+
     }
 
     private fun setupObservers() {
         viewModel.loaderState.observe(viewLifecycleOwner) { loaderState ->
             communicator.showLoader(loaderState)
+        }
+        viewModel.createdUser.observe(viewLifecycleOwner) { createdUser ->
+            if (createdUser) {
+                findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
+            }
+        }
+    }
+
+    private fun requestRegister() {
+        if (binding.emailTIET.text.toString().isNotEmpty()
+            && binding.passwordTIET.text.toString().isNotEmpty()) {
+            isValid = true
+        } else {
+            isValid = false
+        }
+
+        if (isValid) {
+            viewModel.requestRegister(binding.emailTIET.text.toString(),
+                binding.passwordTIET.text.toString()
+            )
+        } else {
+            Toast.makeText(activity, "Please introduce your information", Toast.LENGTH_SHORT).show()
         }
     }
 
